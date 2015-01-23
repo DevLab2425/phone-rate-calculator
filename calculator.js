caeed5f280f7ebceeb3c51c49a17e13cbbd6adb0
Barry, Paul <devlab2425@gmail.com>
2015-01-23 08:04:24 -0500

    Wrapped in a closure to seperate from other scripts

--- calculator.js
+++ calculator.js
@@ -1,18 +1,21 @@
-$('document').ready(function(){
-	function calculateRate(minutes){
-		var FIRST_RATE = parseFloat($('#firstRate').val());
-		var ADDITIONAL_RATE = parseFloat($('#additionalRate').val());
-			
-		minutes = parseInt(minutes);
-	
-		if(minutes <= 0){
-			return 0;
-		}
+(function($){
+	$('document').ready(function(){
+		function calculateRate(minutes){
+			var FIRST_RATE = parseFloat($('#firstRate').val());
+			var ADDITIONAL_RATE = parseFloat($('#additionalRate').val());
+				
+			minutes = parseInt(minutes);
+		
+			if(minutes <= 0){
+				return 0;
+			}
 
-		return FIRST_RATE + ( ADDITIONAL_RATE * (minutes - 1) );
-	}
-	
-	$('#calculateBtn').click(function(){
-		$('#cost').val( '$' + parseFloat(calculateRate($('#minutes').val())).toFixed(2) );
+			return FIRST_RATE + ( ADDITIONAL_RATE * (minutes - 1) );
+		}
+		
+		$('#calculateBtn').click(function(){
+			var cost = parseFloat(calculateRate($('#minutes').val())) || 0;
+			$('#cost').val( cost.toFixed(2) );
+		});
 	});
-});
+})(jQuery);
